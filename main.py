561648fec6473b502cea7069d4fbf53703d42397
cmartin-cay <colin.martin@gmail.com>
2023-10-01 12:25:42 +0100

    Updates

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from databse.db import init_db
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 
 @app.on_event("startup")
 async def start_db():
     await init_db()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/first")
-async def first():
-    return{"message": "first route"}
+
+@app.get("/student", response_class=HTMLResponse)
+async def first(request: Request):
+    return templates.TemplateResponse("add_student.html", {"request": request})
